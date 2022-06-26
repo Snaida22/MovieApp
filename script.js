bdf0ed8cd25f567a6fb9048719e204ae7c594190
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-26 21:06:57 +0300

    bug fix

--- script.js
+++ script.js
@@ -161,50 +161,15 @@ function clearBtn(){
         })
         tagsEl.append(clear);
     }
-} 
-
-getMovies(API_URL);
-
-function getMovies(url) {
-  lastUrl = url;
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results)
-        if(data.results.length !== 0){
-            showMovies(data.results);
-            currentPage = data.page;
-            nextPage = currentPage + 1;
-            prevPage = currentPage - 1;
-            totalPages = data.total_pages;
-
-            current.innerText = currentPage;
-
-            if(currentPage <= 1){
-              prev.classList.add('disabled');
-              next.classList.remove('disabled')
-            }else if(currentPage>= totalPages){
-              prev.classList.remove('disabled');
-              next.classList.add('disabled')
-            }else{
-              prev.classList.remove('disabled');
-              next.classList.remove('disabled')
-            }
-
-            tagsEl.scrollIntoView({behavior : 'smooth'})
-
-        }else{
-            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
-        }
-       
-    })
-
+    
 }
 
 
-getMovies(API_URL);
 
-function getMovies(url) {
+async function getMovies(url) {
   lastUrl = url;
-    fetch(url).then(res => res.json()).then(data => {
+  const response = await fetch(url)
+  const data = await response.json()
         console.log(data.results)
         if(data.results.length !== 0){
             showMovies(data.results);
@@ -232,8 +197,41 @@ function getMovies(url) {
             main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
         }
        
-    })
+  
 
+}
+getMovies(API_URL);
+
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.map(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Learn More</button
+            </div>
+        
+        `
+
+        main.appendChild(movieEl);
+
+        document.getElementById(id).addEventListener('click', () => {
+          console.log(id)
+          openNav(movie)
+        })
+    })
 }
 
 const overlayContent = document.getElementById('overlay-content');
@@ -339,12 +337,12 @@ rightArrow.addEventListener('click', () => {
 
 
 function getColor(vote) {
-    if(vote>= 8){
-        return 'green'
-    }else if(vote >= 5){
-        return "orange"
-    }else{
-        return 'red'
+    if(vote >= 8){
+      return 'green'
+    } else if(vote >= 5){
+      return 'orange'
+    } else{
+      return 'red'
     }
 }
 
@@ -375,20 +373,18 @@ next.addEventListener('click', () => {
 })
 
 function pageCall(page){
-    let urlSplit = lastUrl.split('?');
-    let queryParams = urlSplit[1].split('&');
-    let key = queryParams[queryParams.length -1].split('=');
-    if(key[0] != 'page'){
-      let url = lastUrl + '&page='+page
-      getMovies(url);
-    }else{
-      key[1] = page.toString();
-      let a = key.join('=');
-      queryParams[queryParams.length -1] = a;
-      let b = queryParams.join('&');
-      let url = urlSplit[0] +'?'+ b
-      getMovies(url);
-    }
+  let urlSplit = lastUrl.split('?');
+  let queryParams = urlSplit[1].split('&');
+  let key = queryParams[queryParams.length -1].split('=');
+  if(key[0] != 'page'){
+    let url = lastUrl + '&page='+page
+    getMovies(url);
+  }else{
+    key[1] = page.toString();
+    let a = key.join('=');
+    queryParams[queryParams.length -1] = a;
+    let b = queryParams.join('&');
+    let url = urlSplit[0] +'?'+ b
+    getMovies(url);
   }
-    
-
+}
