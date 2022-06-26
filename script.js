631ae1ed02911e94cffc39debee1cbd807a5bcc5
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-26 20:58:29 +0300

    call the page

--- script.js
+++ script.js
@@ -373,5 +373,22 @@ next.addEventListener('click', () => {
     pageCall(nextPage);
   }
 })
+
+function pageCall(page){
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length -1].split('=');
+    if(key[0] != 'page'){
+      let url = lastUrl + '&page='+page
+      getMovies(url);
+    }else{
+      key[1] = page.toString();
+      let a = key.join('=');
+      queryParams[queryParams.length -1] = a;
+      let b = queryParams.join('&');
+      let url = urlSplit[0] +'?'+ b
+      getMovies(url);
+    }
+  }
     
 
